d051b2b5783a207f875a1df917a03b9046eaf1d1
Johnpittas <35944056+Johnpittas@users.noreply.github.com>
2018-04-08 16:45:27 -0700

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,3 @@
 hist(as.numeric(house_a_data$Global_active_power), col="Red", main="Global Active Power", xlab="Global Active power (kilowatts)", ylab="Frequency")
+dev.copy(device = png, file = "Plot1.png")
+dev.off()
